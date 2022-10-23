98e7f34e162299033921284b48432dff0b11de93
manoel bertin <manoeltirj@gmail.com>
2022-10-23 07:35:15 -0300

    criando as actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,18 @@
 class PostsController < ApplicationController
   def index 
     @posts = Post.order(created_at: :desc)
-
+    
+    # render json: {message: 'Yahoo'}
     render json: @posts
+  end
+
+  def create 
+    @post = Post.new # parametros
+
+    if @post.save 
+      render json: @post, status: :created 
+    else
+      render json: @post.erros, status: :unprocessable_entity
+    end
   end
 end
