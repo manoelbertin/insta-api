53d2ffb58ea5f5a07428f90eae4fbfdeb9a0c483
manoel bertin <manoeltirj@gmail.com>
2022-12-13 10:45:40 -0300

    criado as rotas para tokens e users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :v1, defaults: { format: :json} do 
+    resources :tokens, only: [:create]
+
+    resources :users, only: %i[show create update,]
+  end
   resources :posts do
     resources :comments
   end
