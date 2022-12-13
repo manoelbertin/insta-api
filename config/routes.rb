0761f0fc7c021bfc9ac84316ce2a06946166b293
manoel bertin <manoeltirj@gmail.com>
2022-12-13 10:47:39 -0300

    consertei o update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json} do 
     resources :tokens, only: [:create]
 
-    resources :users, only: %i[show create update,]
+    resources :users, only: %i[show create update]
   end
   resources :posts do
     resources :comments
