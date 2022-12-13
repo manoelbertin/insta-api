833834d1daba533b808b23d723041c6dab2f6419
manoel bertin <manoeltirj@gmail.com>
2022-12-13 09:50:56 -0300

    aqui é pra criar o usuário

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,21 @@
+class V1::UsersController < ApplicationController
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
