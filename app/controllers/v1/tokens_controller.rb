7c1954485e21b7b03198f6495883bcd4bcb684f8
manoel bertin <manoeltirj@gmail.com>
2022-12-13 09:45:26 -0300

    fiz o tokens_controllers

---
+++ app/controllers/v1/tokens_controller.rb
@@ -0,0 +1,19 @@
+class V1::TokensController < ApplicationController
+  def create
+    @user = User.find_by_email(user_params[:email])
+    if @user&authenticate(user_params[:password])
+      render json: {
+        token: JsonWebToken.encode(user_id: @user_id),
+        email: @user.email
+      }
+    else
+      head: unauthorize
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
