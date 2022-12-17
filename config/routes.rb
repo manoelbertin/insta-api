68cb7bd3e89c07864e28ac57772713250c91d993
manoel bertin <manoeltirj@gmail.com>
2022-12-17 10:05:17 -0300

    user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json} do 
     resources :tokens, only: [:create]
 
-    resource :users, only: %i[show create update]
+    resource :user, only: %i[show create update]
   end
   resources :posts do
     resources :comments
